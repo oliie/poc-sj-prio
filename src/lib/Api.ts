58c770ba2f9b44c9143ce2aef6d2cf2ce3322364
Bill Kurtson <bill.kurtson@gmail.com>
2023-05-29 12:22:21 +0200

    #6
    Add generic Api

---
+++ src/lib/Api.ts
@@ -0,0 +1,87 @@
+function getCurrentLanguage() {
+	return 'sv-se';
+}
+
+const headers = new Headers();
+
+function Api(): {
+	init: () => void;
+	get: <ResponseType>(resourcePath: string) => Promise<ResponseType>;
+	post: <ResponseType>(
+		resourcePath: string,
+		requestBody: RequestInit['body']
+	) => Promise<ResponseType>;
+	put: <ResponseType>(
+		resourcePath: string,
+		requestBody: RequestInit['body']
+	) => Promise<ResponseType>;
+	remove: <ResponseType>(resourcePath: string) => Promise<ResponseType>;
+} {
+	async function sendRequest<ResponseType>(
+		resourcePath: string,
+		requestInit: RequestInit
+	): Promise<ResponseType> {
+		const response = await fetch(
+			`${process.env.REACT_APP_API_BASE_URL}${resourcePath}`,
+			requestInit
+		);
+		const responseBody: ResponseType = await response.json();
+
+		if (!response.ok) {
+			// return rejectWithSjApiError(response, responseBody as ErrorResponse);
+			console.error(response, responseBody);
+		}
+
+		return responseBody;
+	}
+
+	function init(): void {
+		headers.set('Content-Type', 'application/json; charset=UTF-8');
+		headers.set('X-api.sj.se-language', getCurrentLanguage());
+	}
+
+	function get<ResponseType>(resourcePath: string): Promise<ResponseType> {
+		return sendRequest<ResponseType>(resourcePath, {
+			headers
+		});
+	}
+
+	function post<ResponseType>(
+		resourcePath: string,
+		requestBody: RequestInit['body']
+	): Promise<ResponseType> {
+		return sendRequest<ResponseType>(resourcePath, {
+			headers,
+			method: 'POST',
+			body: requestBody
+		});
+	}
+
+	function put<ResponseType>(
+		resourcePath: string,
+		requestBody: RequestInit['body']
+	): Promise<ResponseType> {
+		return sendRequest<ResponseType>(resourcePath, {
+			headers,
+			method: 'PUT',
+			body: requestBody
+		});
+	}
+
+	function remove<ResponseType>(resourcePath: string): Promise<ResponseType> {
+		return sendRequest<ResponseType>(resourcePath, {
+			headers,
+			method: 'DELETE'
+		});
+	}
+
+	return {
+		init,
+		get,
+		post,
+		put,
+		remove
+	};
+}
+
+export default Api();
