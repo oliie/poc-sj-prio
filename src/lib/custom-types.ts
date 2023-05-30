257b25a28d261cffed6145bf545c2a77a8eb7138
Oliver Praesto <oliver.praesto@gmail.com>
2023-05-30 14:58:46 +0200

    - adds typings for json content

---
+++ src/lib/custom-types.ts
@@ -0,0 +1,26 @@
+export type Offer = {
+	imgUrl: string;
+	title: string;
+	body: string;
+	points: number;
+	isLimited: boolean;
+};
+
+export type ShopDetails = {
+	title: string;
+	description: string;
+	heroImageUrl: string;
+	offers: Offer[];
+};
+
+export type Shop = {
+	imgUrl: string;
+	title: string;
+	description: string;
+	slug: string;
+};
+
+export type ShopSection = {
+	header: string;
+	shops: Shop[];
+};
