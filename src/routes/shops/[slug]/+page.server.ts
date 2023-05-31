9b53d6565ae37d90183ea1c5b7c025b633658a63
Daniel Termin <daniel.termin@sj.se>
2023-05-31 15:26:22 +0200

    #12: Adds TODO comment about error handling in shops/[slug]/+page.server.ts

--- src/routes/shops/[slug]/+page.server.ts
+++ src/routes/shops/[slug]/+page.server.ts
@@ -4,6 +4,8 @@ import { error } from '@sveltejs/kit';
 export const prerender = true;
 
 export const load: PageServerLoad = async ({ params, fetch }) => {
+	// TODO: errror handling doesn't work at the moment…
+
 	try {
 		const response = await fetch(`/api/shops/${params.slug}`);
 		return await response.json();
