969348a976c86482c0fab5331dbaf76bccee5351
Daniel Termin <daniel.termin@sj.se>
2023-05-31 15:43:02 +0200

    #12: Removes prerender flag from /shops/[slug]/+page.server.ts

--- src/routes/shops/[slug]/+page.server.ts
+++ src/routes/shops/[slug]/+page.server.ts
@@ -1,16 +1,14 @@
 import type { PageServerLoad } from './$types';
 import { error } from '@sveltejs/kit';
 
-export const prerender = true;
-
 export const load: PageServerLoad = async ({ params, fetch }) => {
-	// TODO: errror handling doesn't work at the moment…
+  // TODO: errror handling doesn't work at the moment…
 
-	try {
-		const response = await fetch(`/api/shops/${params.slug}`);
-		return await response.json();
-	} catch (e) {
-		console.log('nothing to see');
-		throw error(404);
-	}
+  try {
+    const response = await fetch(`/api/shops/${params.slug}`);
+    return await response.json();
+  } catch (e) {
+    console.log('nothing to see');
+    throw error(404);
+  }
 };
