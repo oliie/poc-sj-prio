524fd50d7dc5c3363674a623f18755c9cb867956
Bill Kurtson <bill.kurtson@gmail.com>
2023-05-31 14:25:46 +0200

    #3 ShopCard responsiveness
    This commit:
    - resize shops in shop list for different browser widths

--- src/lib/components/ShopCard.svelte
+++ src/lib/components/ShopCard.svelte
@@ -6,7 +6,7 @@
 	export let slug = '';
 </script>
 
-<div class="card card-compact w-80 bg-base-100 shadow-xl rounded-none">
+<div class="card card-compact w-full md:w-56 lg:w-60 2xl:w-80 bg-base-100 shadow-xl rounded-none">
 	<figure><img src={imgUrl} alt={title} /></figure>
 	<div class="card-body">
 		<h2 class="card-title">{title}</h2>
@@ -14,7 +14,7 @@
 		<div class="card-actions justify-start">
 			<a
 				href="/{slug}"
-				class="btn btn-primary bg-sj-leaf-dark hover:bg-sj-leaf-hover-dark rounded-none border-none"
+				class="btn btn-primary bg-sj-leaf-dark hover:bg-sj-leaf-hover-dark rounded-none border-none normal-case"
 				>{buttonText}</a
 			>
 		</div>
