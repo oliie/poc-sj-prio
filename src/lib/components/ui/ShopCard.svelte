8d52aa5b79f707de1bbf6797287d918697d4e7e4
Oliver Praesto <oliver.praesto@gmail.com>
2023-05-31 16:03:35 +0200

    - fixes linking

--- src/lib/components/ui/ShopCard.svelte
+++ src/lib/components/ui/ShopCard.svelte
@@ -6,15 +6,15 @@
   export let slug: string;
 </script>
 
-<div class="card card-compact w-full bg-base-100 shadow-xl rounded-none">
+<div class="w-full rounded-none shadow-xl card card-compact bg-base-100">
   <figure><img src={imgUrl} alt={title} /></figure>
   <div class="card-body">
     <h2 class="card-title">{title}</h2>
     <p>{description}</p>
-    <div class="card-actions justify-start">
+    <div class="justify-start card-actions">
       <a
-        href="poangshop/{slug}"
-        class="btn btn-primary bg-sj-leaf-dark hover:bg-sj-leaf-hover-dark rounded-none border-none normal-case"
+        href="shops/{slug}"
+        class="normal-case border-none rounded-none btn btn-primary bg-sj-leaf-dark hover:bg-sj-leaf-hover-dark"
         >{buttonText}</a
       >
     </div>
