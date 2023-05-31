3a3b127c54feb3cc5f2605746690d50720f47320
Daniel Termin <daniel.termin@sj.se>
2023-05-31 10:56:59 +0200

    #2 Adds Hero component. Adds component for showing current prio points. Adds rudimentary language util. This might not be the "right" way… Adds .idea to .gitignore.

---
+++ src/lib/utils/languageUtil.ts
@@ -0,0 +1,9 @@
+export enum language {
+	SV = 'sv',
+	EN = 'en'
+}
+
+export const locale = {
+	[language.SV]: 'sv-SE',
+	[language.EN]: 'en-GB'
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
 <script>
-	import Button from '$lib/components/ui/Button.svelte';
+	import Hero from '$lib/components/ui/Hero.svelte';
+	import TotalPointsView from '$lib/components/ui/TotalPointsView.svelte';
+	import { language } from '$lib/utils/languageUtil';
+
+	const heroData = {
+		title: 'First hotels',
+		introText:
+			'Dina SJ Prio-poäng kan växlas in mot värdecheckar som kan användas på alla First Hotels i Sverige.',
+		imageSrc: '/images/hero1.jpg',
+		imageAltText: 'Ett rapsfält under en blå sommarhimmel.'
+	};
 </script>
 
-<Button>SJ Prio Shop</Button>
+<Hero {...heroData}>
+	<TotalPointsView
+		lang={language.SV}
+		points={34000}
+		pointsTitle="Du har"
+		pointsLabel="poäng att handla för"
+	/>
+</Hero>
