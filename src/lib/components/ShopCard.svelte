187f86c20e056c448c95284cbb347a92c2d14f27
Bill Kurtson <bill.kurtson@gmail.com>
2023-05-31 14:15:58 +0200

    #3 Update ShopCard styling
    This commit:
    - update button styling to match SJ colors

--- src/lib/components/ShopCard.svelte
+++ src/lib/components/ShopCard.svelte
@@ -12,7 +12,11 @@
 		<h2 class="card-title">{title}</h2>
 		<p>{description}</p>
 		<div class="card-actions justify-start">
-			<a href="/{slug}" class="btn btn-primary">{buttonText}</a>
+			<a
+				href="/{slug}"
+				class="btn btn-primary bg-sj-leaf-dark hover:bg-sj-leaf-hover-dark rounded-none border-none"
+				>{buttonText}</a
+			>
 		</div>
 	</div>
 </div>
