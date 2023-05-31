62dc19ee2716c1a6c756798d17b6b5712e3542a8
Oliver Praesto <oliver.praesto@gmail.com>
2023-05-31 15:58:04 +0200

    - adds navbar

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,11 @@
+<div class="navbar bg-base-100">
+  <div class="flex-1">
+    <a href="/" class="text-xl normal-case btn btn-ghost text-sj-leaf-dark">SJ Prioshop</a>
+  </div>
+  <div class="justify-end flex-1">
+    <div class="grid items-center grid-cols-2 gap-4">
+      <div><span class="text-sm">Poäng:</span> <b>TODO</b></div>
+      <button class="btn btn-ghost">Logga in</button>
+    </div>
+  </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,11 @@
 <script>
+  import NavBar from '$lib/components/NavBar.svelte';
   import '../app.css';
 </script>
+
+<nav>
+  <NavBar />
+</nav>
 
 <main>
   <slot />
