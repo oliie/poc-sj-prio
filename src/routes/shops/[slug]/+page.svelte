7314ffa658d894ca78c49be2c466dffe89b490c9
Oliver Praesto <oliver.praesto@gmail.com>
2023-06-01 15:36:45 +0200

    - adds dialog WIP

--- src/routes/shops/[slug]/+page.svelte
+++ src/routes/shops/[slug]/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
   import { page } from '$app/stores';
+  import CheckoutDialog from '$lib/components/ui/CheckoutDialog.svelte';
   import Hero from '$lib/components/ui/Hero.svelte';
   import Offers from '$lib/components/ui/Offers.svelte';
+  import type { Offer } from '$lib/custom-types.js';
 
   export let data;
 
+  let show = false;
+  let offer: Offer;
+
   const { title, description, heroImageUrl, heroImageAltText, offers } = data;
-  console.log(offers);
 
   const heroData = {
     title,
@@ -22,6 +26,11 @@
     { property: 'og:image', content: heroImageUrl },
     { property: 'og:url', content: $page.url }
   ];
+
+  const handleOfferSelected = (event: CustomEvent) => {
+    offer = event.detail.offer;
+    show = !show;
+  };
 </script>
 
 <svelte:head>
@@ -34,5 +43,9 @@
 <Hero {...heroData} />
 
 <div class="container p-4 pb-8 mx-auto">
-  <Offers {offers} />
+  <Offers {offers} on:selectOffer={handleOfferSelected} />
 </div>
+
+{#if offer}
+  <CheckoutDialog {offer} bind:show />
+{/if}
