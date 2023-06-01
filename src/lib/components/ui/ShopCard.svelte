d4a944a9b693d4b108980d2ceb6f51296abfca94
Daniel Termin <daniel.termin@sj.se>
2023-06-01 17:04:04 +0200

    #46 Adds full card click functionality.

--- src/lib/components/ui/ShopCard.svelte
+++ src/lib/components/ui/ShopCard.svelte
@@ -1,32 +1,40 @@
 <script lang="ts">
-  import type {HeadingLevel} from "../../custom-types";
+  import { onMount } from 'svelte';
+  import type { HeadingLevel } from '$lib/custom-types';
+
+  let card;
+  let mainLink;
 
   export let title: string;
   export let imgUrl: string;
   export let description: string;
-  export let buttonText: string;
   export let slug: string;
   export let headingLevel: HeadingLevel = 'h3';
+
+  onMount(() => {
+    card.onclick = handleClick;
+    mainLink.onclick = (event) => event.stopPropagation();
+  });
+
+  function handleClick(event) {
+    const isTextSelected = window.getSelection().toString();
+    if (!isTextSelected) {
+      mainLink.click();
+    }
+  }
 </script>
 
-<div class="w-full rounded-none shadow-xl card card-compact bg-base-100">
+<div
+  bind:this={card}
+  class="w-full card card-compact card-bordered rounded-[8px] bg-base-100 hover:shadow-md transition-shadow cursor-pointer"
+>
   <figure>
-    <img
-      src={`//wsrv.nl/?url=${imgUrl}&w=400&output=webp`}
-      alt=""
-      width="400"
-      height="225"
-    />
+    <img src={`//wsrv.nl/?url=${imgUrl}&w=400&output=webp`} alt="" width="400" height="225" />
   </figure>
   <div class="card-body">
-    <svelte:element this={headingLevel} class="card-title">{title}</svelte:element>
+    <svelte:element this={headingLevel} class="card-title"
+      ><a href="shops/{slug}" bind:this={mainLink}>{title}</a></svelte:element
+    >
     <p>{description}</p>
-    <div class="justify-start card-actions">
-      <a
-        href="shops/{slug}"
-        class="normal-case border-none rounded-none btn btn-primary bg-sj-leaf-dark hover:bg-sj-leaf-hover-dark"
-        >{buttonText}</a
-      >
-    </div>
   </div>
 </div>
