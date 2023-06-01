f77001402f0a327c9ab407f26fdf557511dc8535
Oliver Praesto <oliver.praesto@gmail.com>
2023-06-01 14:01:03 +0200

    - fixes start page design

--- src/lib/services/sv-SE.ts
+++ src/lib/services/sv-SE.ts
@@ -1,8 +1,6 @@
 const translations: { [key: string]: string } = {
   start_page_hero_title: 'SJ Prioshop',
   start_page_hero_alt_text: 'Ett rapsfält under en blå sommarhimmel.',
-  start_page_hero_description:
-    'Unna dig eller ge bort något till någon du tycker om. I våra shoppar hittar du allt hotellvistelser och biobiljetter till välgörenhet och hälsa. Välkommen in!',
   start_page_title: 'SJ Prio poängshop',
   start_page_description:
     'I våra poängshoppar kan du använda dina SJ Prio-poäng till tågresor, biobiljetter, ljudböcker, hyrfilm och mycket mer. Unna dig själv eller ge bort till någon som du tycker om. <br><br><i>Vi reserverar oss för att varor kan sälja slut eller bara säljs under en begränsad period.</i> <a href="https://www.sj.se/sv/sj-prio/poang/sj-prio-poangshop.html" target="_blank">Läs villkor för SJ Prio poängshop.</a>'
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 
   const heroData = {
     title: t('start_page_hero_title'),
-    introText: t('start_page_hero_description'),
+    introText: t('start_page_description'),
     imageSrc:
       'https://www.sj.se/content/dam/externt/bilder/natur-landskap/rapsfaltmedskogruntom-2019.jpg/jcr:content/renditions/cq5dam.web.bg.1920.y.jpg',
     imageAltText: t('start_page_hero_alt_text')
@@ -24,21 +24,5 @@
 <Hero {...heroData} />
 
 <div class="container p-4 pb-8 mx-auto">
-  <div class="grid grid-cols-3 my-8">
-    <div class="col-span-3 md:col-span-2">
-      <h1 class="text-4xl font-bold">{t('start_page_title')}</h1>
-      <!-- eslint-disable svelte/no-at-html-tags -->
-      <p class="mt-4 html-content">{@html t('start_page_description')}</p>
-    </div>
-  </div>
-
   <ShopSection {shopList} />
 </div>
-
-<style lang="postcss">
-  .html-content {
-    & :global(a) {
-      @apply text-cyan-600 underline;
-    }
-  }
-</style>
