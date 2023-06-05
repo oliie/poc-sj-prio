e8cac855b71f9d6fa45518ea305ead8bdc943dbc
Oliver Praesto <oliver.praesto@gmail.com>
2023-06-05 15:03:35 +0200

    - breaks out to component

--- src/lib/components/ui/CheckoutSummary.svelte
+++ src/lib/components/ui/CheckoutSummary.svelte
@@ -3,6 +3,7 @@
   import { t } from '$lib/services/i18n';
   import { user } from '$lib/stores/user';
   import { createEventDispatcher } from 'svelte';
+  import SummaryDetails from './SummaryDetails.svelte';
 
   const dispatch = createEventDispatcher();
 
@@ -18,20 +19,7 @@
   <p class="py-4">{t('summary_confirmation_email')} {$user?.email}.</p>
 </div>
 
-<div class="w-full pb-6 mx-auto md:w-1/3">
-  <div class="grid grid-cols-2 py-2">
-    <p>{t('summary_product')}</p>
-    <p class="font-bold text-right">{offer.title}</p>
-  </div>
-  <div class="grid grid-cols-2 py-2">
-    <p>{t('summary_amount')}</p>
-    <p class="font-bold text-right">{amount}</p>
-  </div>
-  <div class="grid grid-cols-2 py-2">
-    <p>{t('summary_points')}</p>
-    <p class="font-bold text-right">{offer.points * amount}</p>
-  </div>
-</div>
+<SummaryDetails {amount} {offer} />
 
 <button
   class="w-full normal-case border-none rounded-none btn btn-primary bg-sj-leaf-dark hover:bg-sj-leaf-hover-dark"
